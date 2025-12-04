a424fe5becb11820dfd8521289d9717795cf7264
Qw3rty26 <asarovalerio26@gmail.com>
2025-12-04 13:23:07 +0100

    added feh and picom flakes

--- systems/laptop/users/qwerty/qwerty_submodules/system/window_managers/flake.nix
+++ systems/laptop/users/qwerty/qwerty_submodules/system/window_managers/flake.nix
@@ -4,13 +4,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    feh.url = "path:./feh";
     i3.url = "path:./i3";
+    picom.url = "path:./picom"; 
   };
 
   outputs = { self, nixpkgs, ... }: {
     homeManagerModule.default = { config, pkgs, ... }: {
       imports = [
+        self.inputs.feh.homeManagerModule.default
         self.inputs.i3.homeManagerModule.default
+        self.inputs.picom.homeManagerModule.default
       ];
     };
   };
