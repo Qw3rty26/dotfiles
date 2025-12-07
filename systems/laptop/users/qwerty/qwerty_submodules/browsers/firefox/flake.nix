e17b93c9bbfa0d4c8a519bcc62a21708519ddba8
Qw3rty26 <asarovalerio26@gmail.com>
2025-12-07 12:04:38 +0100

    updated firefox.flake with some configs

--- systems/laptop/users/qwerty/qwerty_submodules/browsers/firefox/flake.nix
+++ systems/laptop/users/qwerty/qwerty_submodules/browsers/firefox/flake.nix
@@ -9,9 +9,30 @@
 
   outputs = { self, nixpkgs, ... }: {
     homeManagerModule.default = { config, pkgs, ... }: {
-        home.packages = with pkgs; [
-          firefox
-        ];
+      /*home.sessionVariables = {
+        MOZILLA_HOME = "${config.xdg.configHome}/mozilla";  # this doesn't work, let's press F to pay respects...
+      };*/
+      programs.firefox = {
+        enable = true;
+   
+        profiles.default = {
+          settings = {
+            "browser.newtabpage.activity-stream.showSponsored" = false;           # disable ads
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "extensions.pocket.enabled" = false;                                  # disable Pocket
+            "browser.newtabpage.activity-stream.feeds.snippets" = false;          # disable snippets
+            "browser.newtabpage.activity-stream.feeds.system.topstories" = false;
+
+            "ui.systemUsesDarkTheme" = 1;                            # dark mode firefox UI
+            "layout.css.prefers-color-scheme.content-override" = 0;  # dark mode for websites
+
+            "identity.fxaccounts.enabled" = true;      #firefox sync
+            "identity.fxaccounts.autoconnect" = true;
+            "services.sync.enabled" = true;
+            "services.sync.autoconnect" = true;
+          };
+        };
+      };    
     };
   };
 }
